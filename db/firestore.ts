e92fe575b2edbfc124695d1362f66f61b63dac8f
Les Dyman <lesdyman@gmail.com>
2024-10-02 16:31:41 +0200

    hide firebase api keys

--- db/firestore.ts
+++ db/firestore.ts
@@ -1,14 +1,21 @@
-// Імпорт модулів Firebase
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import {
+  FIREBASE_ApiKey,
+  FIREBASE_AuthDomain,
+  FIREBASE_ProjectId,
+  FIREBASE_StorageBucket,
+  FIREBASE_MessagingSenderId,
+  FIREBASE_AppId
+} from '@env';
 
 const configuration = {
-  apiKey: "AIzaSyDASFAmS4w1kTrMGjc2kUgm4h12c-0I-a8",
-  authDomain: "newsapp-47bdb.firebaseapp.com",
-  projectId: "newsapp-47bdb",
-  storageBucket: "newsapp-47bdb.appspot.com",
-  messagingSenderId: "136419846287",
-  appId: "1:136419846287:web:2f33845ca33193bee968fa"
+  apiKey: FIREBASE_ApiKey,
+  authDomain: FIREBASE_AuthDomain,
+  projectId: FIREBASE_ProjectId,
+  storageBucket: FIREBASE_StorageBucket,
+  messagingSenderId: FIREBASE_MessagingSenderId,
+  appId: FIREBASE_AppId,
 };
 
 const app = initializeApp(configuration);
---
+++ env.d.ts
@@ -0,0 +1,8 @@
+declare module '@env' {
+  export const FIREBASE_ApiKey: string;
+  export const FIREBASE_AuthDomain: string;
+  export const FIREBASE_ProjectId: string;
+  export const FIREBASE_StorageBucket: string;
+  export const FIREBASE_MessagingSenderId: string;
+  export const FIREBASE_AppId: string;
+}
